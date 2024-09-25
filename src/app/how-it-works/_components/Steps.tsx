0696856a2e0606944010337352afe11653045e6d
enriconunes <enriconunes02@hotmail.com>
2024-09-25 20:02:05 -0300

    changing images name

--- src/app/how-it-works/_components/Steps.tsx
+++ src/app/how-it-works/_components/Steps.tsx
@@ -4,37 +4,37 @@ const steps = [
   {
     title: "Selecione a quantidade de bilhetes",
     description: "Cada bilhete representa um dia de experiência. Escolha a melhor quantidade para você.",
-    image: "/step-1.png"
+    image: "/step-1.PNG"
   },
   {
     title: "Preencha o formulário principal",
     description: "Forneça as informações necessárias para criar sua experiência especial.",
-    image: "/step-2.png"
+    image: "/step-2.PNG"
   },
   {
     title: "Preencha os bilhetes",
     description: "Clique sobre os bilhetes para preenchê-los com suas mensagens de amor.",
-    image: "/step-3.png"
+    image: "/step-3.PNG"
   },
   {
     title: "Finalize e faça o pagamento",
     description: "Conclua o processo e efetue o pagamento para ativar sua criação.",
-    image: "/step-4.png"
+    image: "/step-4.PNG"
   },
   {
     title: "Receba o link e compartilhe",
     description: "Após o pagamento, você receberá um link no site e no seu email. Confira o lixo eletrônico e o spam. Você também pode usar o QR code apresentado na tela para compartilhar.",
-    image: "/step-5.png"
+    image: "/step-5.PNG"
   },
   {
     title: "Use a palavra secreta para acessar a surpresa",
     description: "Lembre-se que é preciso usar a palavra secreta que você definiu no início sempre que for acessar o conteúdo especial.",
-    image: "/step-6.png"
+    image: "/step-6.PNG"
   },
   {
     title: "Resultado",
     description: "O resultado final será parecido com este exemplo. Observe que desbloqueamos somente um bilhete por dia:",
-    image: "/step-7.png"
+    image: "/step-7.PNG"
   }
 ]
 
